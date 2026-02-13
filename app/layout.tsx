1ec82e41d815302e671d7f2331277247259dba73
Dell Sharma <dell@example.com>
2026-02-13 18:31:31 +0530

    update the rootComponts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+
 import { CartProvider } from "./storecontect/Contectapi";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
